940286106f1efb9bff00a2b00e8d382041c786f4
ianneil.agasen <iann.agasen@gmail.com>
2024-12-25 03:20:42 +0800

    add initial setup for service discovery with Eureka and related configurations

---
+++ backend/infra/discovery/src/test/java/dev/agasen/infra/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.agasen.infra.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
